f828ce0230543e0bcd4ec53d7ab46803de09abca
Jcarden8 <jcardenas29@icloud.com>
2023-08-05 02:43:51 -0700

    message

---
+++ src/components/Top/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <div>
+            <a href="/" style={{marginRight:"10px"}}>Home</a>
+            <a href="/about" style={{marginRight:"10px"}}>About</a>
+            <a href="/episodes" style={{marginRight:"10px"}}>Episodes</a>
+        </div>
+        <button className='theme-button'>Dark Mode</button>
+        </div>
+  )
+}
+
+export default Header
